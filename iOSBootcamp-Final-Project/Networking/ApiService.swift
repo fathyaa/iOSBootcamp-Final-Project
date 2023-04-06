a260f4729f7da724a31b71c9de95fe5a6782e3f0
fathyaa <fathyariyani@gmail.com>
2023-04-06 15:09:44 +0700

    create project

---
+++ iOSBootcamp-Final-Project/Networking/ApiService.swift
@@ -0,0 +1,35 @@
+//
+//  ApiService.swift
+//  iOSBootcamp-Final-Project
+//
+//  Created by Phincon on 06/04/23.
+//
+
+import Foundation
+
+protocol ApiServiceProtocol {
+    mutating func get(url: URL)
+    func callApi<T: Codable>(model: T.Type, completion: @escaping (Result<T, Error>) -> Void)
+}
+
+struct ApiService: ApiServiceProtocol {
+    private var url = URL(string: "")
+    mutating func get(url: URL) {
+        self.url = url
+    }
+    
+    func callApi<T>(model: T.Type, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable, T : Encodable {
+        guard let url = self.url else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let fetchedData = data {
+                do {
+                    let modelData = try JSONDecoder().decode(T.self, from: fetchedData)
+                    return completion(.success(modelData))
+                } catch let error {
+                    print("decode fail")
+                    return completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+}
